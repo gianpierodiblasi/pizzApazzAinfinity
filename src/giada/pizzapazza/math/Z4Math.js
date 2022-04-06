2a80c88cb9ca1947242e391d9edaf06b7dbbaa3f
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-04-06 15:21:28 +0200

    testing

--- src/giada/pizzapazza/math/Z4Math.js
+++ src/giada/pizzapazza/math/Z4Math.js
@@ -5,5 +5,22 @@ class Z4Math {
   static SQUARE_GOLD_SECTION = Z4Math.GOLD_SECTION * Z4Math.GOLD_SECTION;
   static RAD2DEG = 180 / Math.PI;
   static DEG2RAD = Math.PI / 180;
-  a = 0;
+
+  constructor() {
+  }
+
+  static rad2deg(radians) {
+  }
+
+  static deg2rad(degrees) {
+  }
+
+  static distance(x1, y1, x2, y2) {
+  }
+
+  static atan(x0, y0, x, y) {
+  }
+
+  static ripple(value, min, max, ripple) {
+  }
 }
