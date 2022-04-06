4b7155f7e8a7e27acf42f89ff2ac18299268215a
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-04-06 18:02:49 +0200

    testing

--- src/giada/pizzapazza/math/Z4Math.js
+++ src/giada/pizzapazza/math/Z4Math.js
@@ -7,31 +7,31 @@ class Z4Math {
   static DEG2RAD = Math.PI / 180;
 
   constructor() {
-}
 
+  }
 
   static rad2deg(radians) {
-  radians * Z4Math.RAD2DEG;}
-
+    return radians * Z4Math.RAD2DEG;
+  }
 
   static deg2rad(degrees) {
-  degrees * Z4Math.DEG2RAD;}
-
+    return degrees * Z4Math.DEG2RAD;
+  }
 
   static distance(x1, y1, x2, y2) {
-  let x = x1 - x2;;
-  let y = y1 - y2;;
-  Math.sqrt(x * x + y * y);}
-
+    let x = x1 - x2;
+    let y = y1 - y2;
+    return Math.sqrt(x * x + y * y);
+  }
 
   static atan(x0, y0, x, y) {
-  let a = Math.atan2(y - y0, x - x0);;
-  a < 0 ? a + Z4Math.TWO_PI : a;}
-
+    let a = Math.atan2(y - y0, x - x0);
+    return a < 0 ? a + Z4Math.TWO_PI : a;
+  }
 
   static ripple(value, min, max, ripple) {
-  let rnd = (max - min) * ripple * Math.random();;
-  value +=Math.random() > 0.5 ? rnd : -rnd;
-  value < min ? min : value > max ? max : value;}
-
+    let rnd = (max - min) * ripple * Math.random();
+    value += Math.random() > 0.5 ? rnd : -rnd;
+    return value < min ? min : value > max ? max : value;
+  }
 }
