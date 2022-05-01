fcc97bdf34114de1bedc1c25f321a36978437141
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-05-01 20:39:15 +0200

    refactoring

---
+++ deposito/Z4FancifulValue.java
@@ -0,0 +1,9 @@
+package giada.pizzapazza.math;
+
+/**
+ * The fanciful value
+ *
+ * @author gianpiero.di.blasi
+ */
+public class Z4FancifulValue extends Z4AbstractFancifulValue<Z4FancifulValue> {
+}
--- src/giada/pizzapazza/math/Z4FancifulValue.java
+++
@@ -1,9 +0,0 @@
-package giada.pizzapazza.math;
-
-/**
- * The fanciful value
- *
- * @author gianpiero.di.blasi
- */
-public class Z4FancifulValue extends Z4AbstractFancifulValue<Z4FancifulValue> {
-}
