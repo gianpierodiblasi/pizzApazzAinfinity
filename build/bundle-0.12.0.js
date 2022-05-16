ce7ec0dfdad969482eb920cbaff272c51cb1e848
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-05-16 17:17:16 +0200

    refactoring

--- build/bundle-0.12.0.js
+++ build/bundle-0.12.0.js
@@ -2271,28 +2271,30 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
 
    valueSpan = this.querySelector(".fanciful-value-span");
 
-  // private final $HTMLElement uniformCheck = this.querySelector(".uniform-check");
-  // private final HTMLElement toggleUniform = this.querySelector(".toggle-uniform");
-  // private final HTMLElement toggleUniformImg = this.querySelector(".toggle-uniform img");
-  // 
+   uniformCheck = this.querySelector(".uniform-check");
+
    constantUI = new Z4SignedValueUI();
 
    randomUI = new Z4SignedRandomValueUI();
 
    proportionalUI = new Z4SignedValueUI();
 
-  // 
    mutationObserver = new MutationObserver(() => this.setSpan());
 
-  // private boolean constantSignVisible = true;
-  // private boolean randomSignVisible = true;
-  // private boolean proportionalSignVisible = true;
-  // private boolean constantVisible = true;
-  // private boolean randomVisible = true;
-  // private boolean proportionalVisible = true;
+   constantSignVisible = true;
+
+   randomSignVisible = true;
+
+   proportionalSignVisible = true;
+
+   constantVisible = true;
+
+   randomVisible = true;
+
+   proportionalVisible = true;
+
    selector = new Array();
 
-  // 
   static  PATH = Z4Loader.UP + (Z4Loader.allFiles ? "src/image/" : "build/image/");
 
   static  UI = Z4HTMLFactory.get("giada/pizzapazza/math/ui/Z4FancifulValueUI.html");
@@ -2302,50 +2304,61 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    */
   constructor() {
     super(Z4FancifulValueUI.UI);
-    // this.uniformCheck.id = "uniform_" + new Date().getTime() + "_" + parseInt(1000 * Math.random());
-    // this.querySelector(".uniform-label").setAttribute("for", this.uniformCheck.id);
-    // this.uniformCheck.onchange = (event) -> {
-    // this.setUI();
-    // this.onchange.$apply(this.value.setUniformSign(this.uniformCheck.checked));
-    // return null;
-    // };
-    // 
-    // this.toggleUniformImg.setAttribute("src", Z4FancifulValueUI.PATH + "z4sign_" + this.toggleUniform.getAttribute("data-value") + ".svg");
-    // 
-    // NodeList imgs = this.querySelectorAll(".toggle-uniform-dropdown-menu img");
-    // for (int i = 0; i < imgs.length; i++) {
-    // HTMLElement img = (HTMLElement) imgs.item(i);
-    // img.setAttribute("src", Z4FancifulValueUI.PATH + "z4sign_" + img.getAttribute("data-icon") + ".svg");
-    // }
-    // 
-    // NodeList buttons = this.querySelectorAll(".toggle-uniform-dropdown-menu .dropdown-item");
-    // for (int i = 0; i < buttons.length; i++) {
-    // HTMLElement button = (HTMLElement) buttons.item(i);
-    // button.onclick = (event) -> {
-    // String str = button.getAttribute("data-value");
-    // this.toggleUniform.setAttribute("data-value", str);
-    // this.toggleUniformImg.setAttribute("src", Z4FancifulValueUI.PATH + "z4sign_" + str + ".svg");
-    // 
-    // this.constantUI.setValue(this.constantUI.getValue().setSign(this.getUniformSign(str)));
-    // this.onchange.$apply(this.value.setConstant(this.constantUI.getValue()));
-    // return null;
-    // };
-    // }
-    // 
+    this.uniformCheck.id = "uniform_" + new Date().getTime() + "_" + parseInt(1000 * Math.random());
+    this.querySelector(".uniform-label").setAttribute("for", this.uniformCheck.id);
+    this.uniformCheck.onchange = (event) => {
+      this.value.setUniformSign(this.uniformCheck.checked);
+      this.setUI();
+      this.setSpan();
+      this.onchange(this.value);
+      return null;
+    };
+    let imgs = this.querySelectorAll(".btn-group-uniform img");
+    for (let i = 0; i < imgs.length; i++) {
+      let img = imgs.item(i);
+      img.setAttribute("src", Z4FancifulValueUI.PATH + "z4sign_" + img.getAttribute("data-icon") + ".svg");
+    }
+    let buttons = this.querySelectorAll(".btn-group-uniform button");
+    for (let i = 0; i < buttons.length; i++) {
+      let button = buttons.item(i);
+      button.onclick = (event) => {
+        switch(button.getAttribute("data-value")) {
+          case "positive":
+            this.constantUI.setValue(this.constantUI.getValue().setSign(Z4Sign.POSITIVE));
+            break;
+          case "negative":
+            this.constantUI.setValue(this.constantUI.getValue().setSign(Z4Sign.NEGATIVE));
+            break;
+          case "random":
+            this.constantUI.setValue(this.constantUI.getValue().setSign(Z4Sign.RANDOM));
+            break;
+          case "alternate":
+            this.constantUI.setValue(this.constantUI.getValue().setSign(Z4Sign.alternate()));
+            break;
+        }
+        this.value.setConstant(this.constantUI.getValue());
+        this.setUI();
+        this.setSpan();
+        this.onchange(this.value);
+        return null;
+      };
+    }
     this.constantUI.appendToElement(this.querySelector(".fanciful-costant"));
     this.constantUI.setValueLabel("CONSTANT", false, true);
     this.randomUI.appendToElement(this.querySelector("div.fanciful-random"));
     this.randomUI.setValueLabel("RANDOM", false, true);
     this.proportionalUI.appendToElement(this.querySelector(".fanciful-proportional"));
     this.proportionalUI.setValueLabel("PROPORTIONAL", false, true);
-    // 
-    // this.constantUI.oninput = (event) -> this.onInput();
-    // this.randomUI.oninput = (event) -> this.onInput();
-    // this.proportionalUI.oninput = (event) -> this.onInput();
-    // this.constantUI.onchange = (event) -> this.onChange();
-    // this.randomUI.onchange = (event) -> this.onChange();
-    // this.proportionalUI.onchange = (event) -> this.onChange();
-    // 
+    this.constantUI.oninput = (event) => this.onInput();
+    this.randomUI.oninput = (event) => this.onInput();
+    this.proportionalUI.oninput = (event) => this.onInput();
+    this.constantUI.onchange = (event) => this.onChange();
+    this.randomUI.onchange = (event) => this.onChange();
+    this.proportionalUI.onchange = (event) => this.onChange();
+    let spans = this.querySelectorAll(".value-span");
+    for (let i = 0; i < spans.length; i++) {
+      (spans.item(i)).style.display = "none";
+    }
     let array = new Array("class");
     let observerConfig = new Object();
     observerConfig["attributes"] = true;
@@ -2355,24 +2368,16 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
   }
 
    onInput() {
-    // this.setUniformSign(this.constantUI.getValue().getSign());
-    // 
-    // this.oninput.$apply(this.value.
-    // setConstant(this.constantUI.getValue()).
-    // setRandom(this.randomUI.getValue()).
-    // setProportional(this.proportionalUI.getValue())
-    // );
+    this.value.setConstant(this.constantUI.getValue()).setRandom(this.randomUI.getValue()).setProportional(this.proportionalUI.getValue());
+    this.setSpan();
+    this.oninput(this.value);
     return null;
   }
 
    onChange() {
-    // this.setUniformSign(this.constantUI.getValue().getSign());
-    // 
-    // this.onchange.$apply(this.value.
-    // setConstant(this.constantUI.getValue()).
-    // setRandom(this.randomUI.getValue()).
-    // setProportional(this.proportionalUI.getValue())
-    // );
+    this.value.setConstant(this.constantUI.getValue()).setRandom(this.randomUI.getValue()).setProportional(this.proportionalUI.getValue());
+    this.setSpan();
+    this.onchange(this.value);
     return null;
   }
 
@@ -2399,11 +2404,11 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return This Z4FancifulValueUI
    */
    setComponentsVisible(constant, random, proportional) {
-    // this.constantVisible = constant;
-    // this.randomVisible = random;
-    // this.proportionalVisible = proportional;
-    // 
+    this.constantVisible = constant;
+    this.randomVisible = random;
+    this.proportionalVisible = proportional;
     this.setUI();
+    this.setSpan();
     return this;
   }
 
@@ -2417,11 +2422,11 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return
    */
    setSignsVisible(constant, random, proportional) {
-    // this.constantSignVisible = constant;
-    // this.randomSignVisible = random;
-    // this.proportionalSignVisible = proportional;
-    // 
+    this.constantSignVisible = constant;
+    this.randomSignVisible = random;
+    this.proportionalSignVisible = proportional;
     this.setUI();
+    this.setSpan();
     return this;
   }
 
@@ -2433,7 +2438,7 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return This Z4FancifulValueUI
    */
    setConstantRange(min, max) {
-    // this.constantUI.setRange(min, max);
+    this.constantUI.setRange(min, max);
     return this;
   }
 
@@ -2445,7 +2450,7 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return This Z4FancifulValueUI
    */
    setRandomRange(min, max) {
-    // this.randomUI.setRange(min, max);
+    this.randomUI.setRange(min, max);
     return this;
   }
 
@@ -2457,7 +2462,7 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return This Z4FancifulValueUI
    */
    setRandomLengthRange(min, max) {
-    // this.randomUI.setLengthRange(min, max);
+    this.randomUI.setLengthRange(min, max);
     return this;
   }
 
@@ -2469,7 +2474,7 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return This Z4FancifulValueUI
    */
    setProportionalRange(min, max) {
-    // this.proportionalUI.setRange(min, max);
+    this.proportionalUI.setRange(min, max);
     return this;
   }
 
@@ -2482,106 +2487,84 @@ class Z4FancifulValueUI extends Z4AbstractComponentWithValueUI {
    * @return This Z4FancifulValueUI
    */
    setValueLabel(token, bold, italic) {
-    // $HTMLElement valueLabel = this.querySelector(".fanciful-label");
-    // valueLabel.setAttribute("data-token-lang-inner_text", token);
-    // valueLabel.innerHTML = Z4MessageFactory.get(token);
-    // valueLabel.style.fontWeight = bold ? "700" : "400";
-    // valueLabel.style.fontStyle = italic ? "italic" : "normal";
+    let valueLabel = this.querySelector(".fanciful-label");
+    valueLabel.setAttribute("data-token-lang-inner_text", token);
+    valueLabel.innerHTML = Z4MessageFactory.get(token);
+    valueLabel.style.fontWeight = bold ? "700" : "400";
+    valueLabel.style.fontStyle = italic ? "italic" : "normal";
     return this;
   }
 
    setValue(value) {
     this.value = value;
-    // 
-    // this.uniformCheck.checked = this.value.isUniformSign();
-    // 
-    // this.constantUI.setValue(this.value.getConstant());
-    // this.setUniformSign(this.value.getConstant().getSign());
-    // this.randomUI.setValue(this.value.getRandom());
-    // this.proportionalUI.setValue(this.value.getProportional());
-    // 
+    this.uniformCheck.checked = this.value.isUniformSign();
+    this.constantUI.setValue(this.value.getConstant());
+    this.randomUI.setValue(this.value.getRandom());
+    this.proportionalUI.setValue(this.value.getProportional());
     this.setUI();
     this.setSpan();
     return this;
   }
 
-  // 
    setUI() {
-    // this.selector.forEach(sel -> {
-    // this.querySelector(".fanciful-label").classList.remove(sel);
-    // this.querySelector(".form-check").classList.remove(sel);
-    // this.querySelector(".fanciful-container").classList.remove(sel);
-    // });
-    // 
-    // this.selector = new Array<>(
-    // "cv-" + this.constantVisible,
-    // "rv-" + this.randomVisible,
-    // "pv-" + this.proportionalVisible,
-    // "csv-" + this.constantSignVisible,
-    // "rsv-" + this.randomSignVisible,
-    // "psv-" + this.proportionalSignVisible,
-    // "u-" + this.uniformCheck.checked
-    // );
-    // 
-    // this.selector.forEach(sel -> {
-    // this.querySelector(".fanciful-label").classList.add(sel);
-    // this.querySelector(".form-check").classList.add(sel);
-    // this.querySelector(".fanciful-container").classList.add(sel);
-    // });
+    this.selector.forEach(sel => {
+      this.querySelector(".fanciful-container").classList.remove(sel);
+    });
+    this.selector = new Array("cv-" + this.constantVisible, "rv-" + this.randomVisible, "pv-" + this.proportionalVisible, "csv-" + this.constantSignVisible, "rsv-" + this.randomSignVisible, "psv-" + this.proportionalSignVisible, "u-" + this.uniformCheck.checked);
+    this.selector.forEach(sel => {
+      this.querySelector(".fanciful-container").classList.add(sel);
+    });
   }
 
-  // 
-   setUniformSign(sign) {
-    // String str;
-    // 
-    // if (sign == Z4Sign.POSITIVE) {
-    // str = "positive";
-    // } else if (sign == Z4Sign.NEGATIVE) {
-    // str = "negative";
-    // } else if (sign == Z4Sign.RANDOM) {
-    // str = "random";
-    // } else {
-    // str = "alternate";
-    // }
-    // 
-    // this.toggleUniform.setAttribute("data-value", str);
-    // this.toggleUniformImg.setAttribute("src", Z4FancifulValueUI.PATH + "z4sign_" + str + ".svg");
-  }
-
-  // 
-  // private Z4Sign getUniformSign(String str) {
-  // switch (str) {
-  // case "positive":
-  // return Z4Sign.POSITIVE;
-  // case "negative":
-  // return Z4Sign.NEGATIVE;
-  // case "random":
-  // return Z4Sign.RANDOM;
-  // case "alternate":
-  // return Z4Sign.alternate();
-  // default:
-  // return null;
-  // }
-  // }
-  // 
    setSpan() {
-    // if (!this.signVisible) {
-    // this.valueSpan.innerHTML = "" + this.value.getValue();
-    // } else if (this.value.getSign() == Z4Sign.POSITIVE) {
-    // this.valueSpan.innerHTML = "&plus;" + this.value.getValue();
-    // } else if (this.value.getSign() == Z4Sign.NEGATIVE) {
-    // this.valueSpan.innerHTML = "&minus;" + this.value.getValue();
-    // } else if (this.value.getSign() == Z4Sign.RANDOM) {
-    // this.valueSpan.innerHTML = "&plusmn;" + this.value.getValue();
-    // } else {
-    // this.valueSpan.innerHTML = "&plusmn;<sup>&UpArrowDownArrow;</sup>" + this.value.getValue();
-    // }
     if (Z4Setting.isLiteMode()) {
       this.valueSpan.innerHTML = "10 lite";
     } else if (Z4Setting.isStandardMode()) {
       this.valueSpan.innerHTML = "10 standard";
     } else if (Z4Setting.isProMode()) {
-      this.valueSpan.innerHTML = "10 pro";
+      if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && this.randomSignVisible && this.proportionalSignVisible && this.uniformCheck.checked) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + "[" + this.constantUI.getValue().getValue() + "&plus;" + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + "&plus;" + "prn(" + this.proportionalUI.getValue().getValue() + ")" + "]";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && this.randomSignVisible && this.proportionalSignVisible && !this.uniformCheck.checked) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + this.constantUI.getValue().getValue() + this.decodeSign(this.randomUI.getValue().getSign()) + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + this.decodeSign(this.proportionalUI.getValue().getSign()) + "prn(" + this.proportionalUI.getValue().getValue() + ")";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && this.randomSignVisible && !this.proportionalSignVisible && this.uniformCheck.checked) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + "[" + this.constantUI.getValue().getValue() + "&plus;" + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + "&plus;" + "prn(" + this.proportionalUI.getValue().getValue() + ")" + "]";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && this.randomSignVisible && !this.proportionalSignVisible && !this.uniformCheck.checked) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + this.constantUI.getValue().getValue() + this.decodeSign(this.randomUI.getValue().getSign()) + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + "&plus;" + "prn(" + this.proportionalUI.getValue().getValue() + ")";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && !this.randomSignVisible && this.proportionalSignVisible && this.uniformCheck.checked) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + "[" + this.constantUI.getValue().getValue() + "&plus;" + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + "&plus;" + "prn(" + this.proportionalUI.getValue().getValue() + ")" + "]";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && !this.randomSignVisible && this.proportionalSignVisible && !this.uniformCheck.checked) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + this.constantUI.getValue().getValue() + "&plus;" + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + this.decodeSign(this.proportionalUI.getValue().getSign()) + "prn(" + this.proportionalUI.getValue().getValue() + ")";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && this.constantSignVisible && !this.randomSignVisible && !this.proportionalSignVisible) {
+        this.valueSpan.innerHTML = this.decodeSign(this.constantUI.getValue().getSign()) + "[" + this.constantUI.getValue().getValue() + "&plus;" + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + "&plus;" + "prn(" + this.proportionalUI.getValue().getValue() + ")" + "]";
+      } else if (this.constantVisible && this.randomVisible && this.proportionalVisible && !this.constantSignVisible && this.randomSignVisible && this.proportionalSignVisible) {
+        this.valueSpan.innerHTML = "&plus;" + this.constantUI.getValue().getValue() + this.decodeSign(this.randomUI.getValue().getSign()) + this.decodeRandom(this.randomUI.getValue()) + "(" + this.randomUI.getValue().getValue() + ")" + this.decodeSign(this.proportionalUI.getValue().getSign()) + "prn(" + this.proportionalUI.getValue().getValue() + ")";
+      }
+    }
+  }
+
+   decodeSign(sign) {
+    if (sign === Z4Sign.POSITIVE) {
+      return "&plus;";
+    } else if (sign === Z4Sign.NEGATIVE) {
+      return "&minus;";
+    } else if (sign === Z4Sign.RANDOM) {
+      return "&plusmn;";
+    } else {
+      return "&plusmn;<sup>&UpArrowDownArrow;</sup>";
+    }
+  }
+
+   decodeRandom(value) {
+    if (value.isClassic()) {
+      return "rnd";
+    } else if (value.isBezier()) {
+      return "rnd&#8767;<sup>" + value.getLength() + "</sup>";
+    } else if (value.isPolyline()) {
+      return "rnd&#8896;<sup>" + value.getLength() + "</sup>";
+    } else if (value.isStepped()) {
+      return "rnd&#8851;<sup>" + value.getLength() + "</sup>";
+    } else {
+      return "";
     }
   }
 
