1a9fc88b8ba27e2814bb90979118405c52277371
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-06-08 08:19:06 +0200

    demo

--- build/bundle-0.22.0.js
+++ build/bundle-0.22.0.js
@@ -4091,6 +4091,90 @@ class Z4CenteredFigurePainter extends Z4Painter {
   }
 }
 /**
+ * The abstract component to edit a Z4Painter
+ *
+ * @author gianpiero.di.blasi
+ * @param <S>
+ */
+class Z4PainterUI extends Z4AbstractComponentWithValueUI {
+
+   canvas = this.querySelector(".painter-canvas");
+
+   ctx = this.canvas.getContext("2d");
+
+   pointIterator = new Z4Stamper();
+
+   gradientColor = new Z4GradientColor();
+
+   resizeObserver = new ResizeObserver(() => this.drawCanvas());
+
+   mutationObserver = new MutationObserver(() => this.drawCanvas());
+
+  /**
+   * Creates a Z4PainterUI
+   *
+   * @param ui The HTML
+   */
+  constructor(ui) {
+    super(ui);
+    this.initDevicePixelRatio(() => this.drawCanvas());
+    this.resizeObserver.observe(this.canvas);
+    let config = new Object();
+    config["attributeFilter"] = new Array("class");
+    this.mutationObserver.observe(document.body, config);
+  }
+
+  /**
+   * Sets the Z4PointIterator to draw the demo
+   *
+   * @param <T>
+   * @param pointIterator The Z4PointIterator
+   * @return This Z4PainterUI
+   */
+   setPointIterator(pointIterator) {
+    this.pointIterator = pointIterator;
+    this.drawCanvas();
+    return this;
+  }
+
+  /**
+   * Sets the Z4GradientColor to draw the demo
+   *
+   * @param <T>
+   * @param gradientColor The Z4GradientColor
+   * @return This Z4PainterUI
+   */
+   setGradientColor(gradientColor) {
+    this.gradientColor = gradientColor;
+    this.drawCanvas();
+    return this;
+  }
+
+  /**
+   * Draws the demo canvas
+   */
+   drawCanvas() {
+    if (this.canvas.clientWidth) {
+      this.canvas.width = Math.floor(this.canvas.clientWidth * window.devicePixelRatio);
+      this.canvas.height = Math.floor(this.canvas.clientHeight * window.devicePixelRatio);
+      let offscreen = new OffscreenCanvas(this.canvas.clientWidth, this.canvas.clientHeight);
+      let offscreenCtx = offscreen.getContext("2d");
+      this.pointIterator.drawDemo(offscreenCtx, this.value, this.gradientColor, this.canvas.clientWidth, this.canvas.clientHeight);
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.save();
+      this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+      this.ctx.drawImage(offscreen, 0, 0);
+      this.ctx.restore();
+    }
+  }
+
+   dispose() {
+    this.disposeDevicePixelRatio();
+    this.resizeObserver.unobserve(this.canvas);
+    this.mutationObserver.unobserve(document.body);
+  }
+}
+/**
  * The component to edit a Z4Shape2DPainter
  *
  * @author gianpiero.di.blasi
@@ -4991,6 +5075,8 @@ class Z4PointIteratorUI extends Z4AbstractComponentWithValueUI {
 
    painter = null;
 
+   gradientColor = new Z4GradientColor();
+
    resizeObserver = new ResizeObserver(() => this.drawCanvas());
 
    mutationObserver = new MutationObserver(() => this.drawCanvas());
@@ -5050,6 +5136,19 @@ class Z4PointIteratorUI extends Z4AbstractComponentWithValueUI {
     return this;
   }
 
+  /**
+   * Sets the Z4GradientColor to draw the demo
+   *
+   * @param <T>
+   * @param gradientColor The Z4GradientColor
+   * @return This Z4PointIteratorUI
+   */
+   setGradientColor(gradientColor) {
+    this.gradientColor = gradientColor;
+    this.drawCanvas();
+    return this;
+  }
+
    setValue(value) {
     this.value = value;
     this.rotation.setValue(this.value.getRotation());
@@ -5067,7 +5166,7 @@ class Z4PointIteratorUI extends Z4AbstractComponentWithValueUI {
       this.canvas.height = Math.floor(this.canvas.clientHeight * window.devicePixelRatio);
       let offscreen = new OffscreenCanvas(this.canvas.clientWidth, this.canvas.clientHeight);
       let offscreenCtx = offscreen.getContext("2d");
-      this.value.drawDemo(offscreenCtx, this.painter ? this.painter : this.arrowPainter, null, this.canvas.clientWidth, this.canvas.clientHeight);
+      this.value.drawDemo(offscreenCtx, this.painter ? this.painter : this.arrowPainter, this.gradientColor, this.canvas.clientWidth, this.canvas.clientHeight);
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.ctx.save();
       this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
