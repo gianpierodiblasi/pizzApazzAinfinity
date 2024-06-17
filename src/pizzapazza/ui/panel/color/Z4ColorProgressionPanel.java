1b4a92d5be64f9b729ebc9cc1e8d6a2619cf3a3a
gianpiero.diblasi <gianpiero.diblasi@PC-GianpieroDiBlasi>
2024-06-17 09:11:57 +0200

    color progression

--- src/pizzapazza/ui/panel/color/Z4ColorProgressionPanel.java
+++ src/pizzapazza/ui/panel/color/Z4ColorProgressionPanel.java
@@ -2,6 +2,8 @@ package pizzapazza.ui.panel.color;
 
 import def.js.Array;
 import def.js.Object;
+import javascript.awt.BoxLayout;
+import javascript.awt.GBC;
 import javascript.awt.GridBagLayout;
 import javascript.swing.ButtonGroup;
 import javascript.swing.JSPanel;
@@ -17,6 +19,7 @@ import pizzapazza.painter.Z4Painter;
 import pizzapazza.ui.panel.Z4AbstractValuePanel;
 import pizzapazza.util.Z4EmptyImageProducer;
 import pizzapazza.util.Z4Translations;
+import pizzapazza.util.Z4UI;
 import static simulation.js.$Globals.parseInt;
 
 /**
@@ -47,9 +50,37 @@ public class Z4ColorProgressionPanel extends Z4AbstractValuePanel<Z4ColorProgres
     ButtonGroup buttonGroup = new ButtonGroup();
 
     if (orientation == Z4ColorProgressionPanelOrientation.HORIZONTALLY_COMPACT) {
+      Z4UI.addLabel(this, Z4Translations.FILLING, new GBC(0, 0).a(GBC.WEST));
+      panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
+      this.add(panel, new GBC(1, 0).w(3));
+      this.addRadio(Z4ColorProgressionBehavior.SPATIAL, panel, buttonGroup, "left");
+      this.addRadio(Z4ColorProgressionBehavior.TEMPORAL, panel, buttonGroup, "centerh");
+      this.addRadio(Z4ColorProgressionBehavior.RELATIVE_TO_PATH, panel, buttonGroup, "centerh");
+      this.addRadio(Z4ColorProgressionBehavior.RANDOM, panel, buttonGroup, "right");
+
+      Z4UI.addLabel(this, Z4Translations.STEP, new GBC(0, 1).a(GBC.WEST).wx(1));
+      this.add(this.temporalStepSpinner, new GBC(1, 1).w(3).a(GBC.EAST).i(1, 0, 0, 0));
+      this.add(this.temporalStepSlider, new GBC(0, 2).w(4));
+
+      Z4UI.addLabel(this, Z4Translations.LIGHTING, new GBC(0, 3).a(GBC.EAST).w(2).wx(1).i(0, 0, 0, 1));
+      this.add(this.lightingPanel, new GBC(2, 3).w(2).a(GBC.EAST));
     } else if (orientation == Z4ColorProgressionPanelOrientation.VERTICALLY_COMPACT) {
+      Z4UI.addLabel(this, Z4Translations.FILLING, new GBC(0, 0).w(4).a(GBC.WEST));
+      panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
+      this.add(panel, new GBC(0, 1).h(3).i(0, 0, 0, 1));
+      this.addRadio(Z4ColorProgressionBehavior.SPATIAL, panel, buttonGroup, "top");
+      this.addRadio(Z4ColorProgressionBehavior.TEMPORAL, panel, buttonGroup, "centerv");
+      this.addRadio(Z4ColorProgressionBehavior.RELATIVE_TO_PATH, panel, buttonGroup, "centerv");
+      this.addRadio(Z4ColorProgressionBehavior.RANDOM, panel, buttonGroup, "bottom");
+
+      Z4UI.addLabel(this, Z4Translations.STEP, new GBC(1, 1).a(GBC.WEST).wx(1));
+      this.add(this.temporalStepSpinner, new GBC(2, 1).w(2).a(GBC.EAST));
+      this.add(this.temporalStepSlider, new GBC(1, 2).w(3));
+
+      Z4UI.addLabel(this, Z4Translations.LIGHTING, new GBC(2, 3).a(GBC.EAST).i(0, 0, 0, 1));
+      this.add(this.lightingPanel, new GBC(3, 3));
     }
 
     this.temporalStepSpinner.setModel(new SpinnerNumberModel(1, 1, 100, 1));
