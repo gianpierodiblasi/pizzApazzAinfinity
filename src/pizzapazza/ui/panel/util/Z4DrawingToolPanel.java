b0a286af0f1d05a5d8e6c7415aee95d64422c168
gianpiero.diblasi <gianpiero.diblasi@PC-GianpieroDiBlasi>
2024-06-24 12:20:21 +0200

    drawing tool panel

--- src/pizzapazza/ui/panel/util/Z4DrawingToolPanel.java
+++ src/pizzapazza/ui/panel/util/Z4DrawingToolPanel.java
@@ -1,8 +1,5 @@
 package pizzapazza.ui.panel.util;
 
-import javascript.awt.GridBagLayout;
-import javascript.swing.JSPanel;
-import javascript.swing.JSTabbedPane;
 import pizzapazza.ui.panel.Z4AbstractValuePanel;
 import pizzapazza.util.Z4DrawingTool;
 
@@ -17,25 +14,7 @@ public class Z4DrawingToolPanel extends Z4AbstractValuePanel<Z4DrawingTool> {
     super();
     this.cssAddClass("z4drawingtoolpanel");
 
-    JSTabbedPane pane = new JSTabbedPane();
-    pane.setTabPlacement(JSTabbedPane.LEFT);
-    this.add(pane, null);
     
-    JSPanel panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("POINT ITERATOR", panel);
-
-    panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("PAINTER", panel);
-
-    panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("SPATIO-TEMPORAL COLOR", panel);
-
-    panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("TRY ME", panel);
   }
 
   @Override
--- transpile/pizzapazza/ui/panel/util/Z4DrawingToolPanel.js
+++ transpile/pizzapazza/ui/panel/util/Z4DrawingToolPanel.js
@@ -8,21 +8,6 @@ class Z4DrawingToolPanel extends Z4AbstractValuePanel {
   constructor() {
     super();
     this.cssAddClass("z4drawingtoolpanel");
-    let pane = new JSTabbedPane();
-    pane.setTabPlacement(JSTabbedPane.LEFT);
-    this.add(pane, null);
-    let panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("POINT ITERATOR", panel);
-    panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("PAINTER", panel);
-    panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("SPATIO-TEMPORAL COLOR", panel);
-    panel = new JSPanel();
-    panel.setLayout(new GridBagLayout());
-    pane.addTab("TRY ME", panel);
   }
 
    setValue(value) {
