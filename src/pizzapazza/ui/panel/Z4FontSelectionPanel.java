cc033d06782ee44fba73d211c6818f18d09372c0
gianpiero.diblasi <gianpiero.diblasi@PC-GianpieroDiBlasi>
2024-07-12 14:42:12 +0200

    draw text

--- src/pizzapazza/ui/panel/Z4FontSelectionPanel.java
+++ src/pizzapazza/ui/panel/Z4FontSelectionPanel.java
@@ -10,6 +10,11 @@ import javascript.swing.JSLabel;
 import javascript.swing.JSPanel;
 import javascript.swing.JSRadioButton;
 import javascript.swing.JSTextField;
+import pizzapazza.math.Z4Sign;
+import pizzapazza.math.Z4SignBehavior;
+import pizzapazza.math.Z4SignedValue;
+import pizzapazza.ui.panel.math.Z4SignedValuePanel;
+import pizzapazza.ui.panel.math.Z4SignedValuePanelOrientation;
 import pizzapazza.util.Z4Font;
 import pizzapazza.util.Z4Translations;
 import pizzapazza.util.Z4UI;
@@ -23,9 +28,10 @@ public class Z4FontSelectionPanel extends Z4AbstractValuePanel<Z4Font> {
 
   private final JSTextField filter = new JSTextField();
   private final Array<JSRadioButton> radios = new Array<>();
+  private final Z4SignedValuePanel size = new Z4SignedValuePanel(Z4SignedValuePanelOrientation.HORIZONTAL);
+  private final JSLabel sample = new JSLabel();
   private final JSCheckBox bold = new JSCheckBox();
   private final JSCheckBox italic = new JSCheckBox();
-  private final JSLabel sample = new JSLabel();
 
   private final Array<String> fonts;
 
@@ -34,6 +40,7 @@ public class Z4FontSelectionPanel extends Z4AbstractValuePanel<Z4Font> {
    *
    * @param fonts The available fonts
    */
+  @SuppressWarnings("IndexOfReplaceableByContains")
   public Z4FontSelectionPanel(Array<String> fonts) {
     super();
     this.setLayout(new GridBagLayout());
@@ -44,16 +51,14 @@ public class Z4FontSelectionPanel extends Z4AbstractValuePanel<Z4Font> {
     Z4UI.addLabel(this, Z4Translations.FILTER, new GBC(0, 0).a(GBC.WEST));
 
     this.filter.addActionListener(event -> {
-      String str = this.filter.getText();
+      String str = this.filter.getText().toLowerCase();
       this.radios.forEach((radio, index, array) -> {
         radio.setSelected(false);
-        radio.getStyle().display = this.fonts.$get(index).contains(str) ? "flex" : "none";
-
-        this.value = null;
-        this.onchange();
+        radio.getStyle().display = this.fonts.$get(index).toLowerCase().indexOf(str) != -1 ? "flex" : "none";
       });
+      this.onFontChange();
     });
-    this.add(this.filter, new GBC(0, 1).f(GBC.HORIZONTAL));
+    this.add(this.filter, new GBC(0, 1).f(GBC.HORIZONTAL).i(0, 0, 5, 0));
 
     JSPanel panel = new JSPanel();
     panel.cssAddClass("z4fontselectionpanel-fontlist");
@@ -62,17 +67,34 @@ public class Z4FontSelectionPanel extends Z4AbstractValuePanel<Z4Font> {
 
     ButtonGroup buttonGroup = new ButtonGroup();
     fonts.forEach(font -> this.addFont(font, buttonGroup, panel));
+
+    this.size.setSignVisible(false);
+    this.size.setRange(7, 400);
+    this.size.setValue(new Z4SignedValue(new Z4Sign(Z4SignBehavior.POSITIVE), 12));
+    this.size.addChangeListener(event -> this.onFontChange());
+    this.add(this.size, new GBC(1, 0).h(2));
   }
 
   private void addFont(String font, ButtonGroup buttonGroup, JSPanel panel) {
     JSRadioButton radio = new JSRadioButton();
     radio.setText(font);
-    radio.addActionListener(event -> {
-//this.value = new Z4Font(font, 0, true, true);
-      this.onchange();
-    });
+    radio.addActionListener(event -> this.onFontChange());
+
+    this.radios.push(radio);
     buttonGroup.add(radio);
     panel.add(radio, null);
+  }
+
+  @SuppressWarnings("StringEquality")
+  private void onFontChange() {
+    int index = this.radios.findIndex(radio -> radio.isSelected() && radio.getStyle().display == "flex");
+    if (index != -1) {
+      this.value = new Z4Font(this.fonts.$get(index), (int) this.size.getValue().getValue(), this.bold.isSelected(), this.italic.isSelected());
+    } else {
+      this.value = null;
+    }
+
+    this.onchange();
   }
 
   @Override
--- transpile/pizzapazza/ui/panel/Z4FontSelectionPanel.js
+++ transpile/pizzapazza/ui/panel/Z4FontSelectionPanel.js
@@ -9,11 +9,13 @@ class Z4FontSelectionPanel extends Z4AbstractValuePanel {
 
    radios = new Array();
 
+   size = new Z4SignedValuePanel(Z4SignedValuePanelOrientation.HORIZONTAL);
+
+   sample = new JSLabel();
+
    bold = new JSCheckBox();
 
    italic = new JSCheckBox();
-
-   sample = new JSLabel();
 
    fonts = null;
 
@@ -29,32 +31,44 @@ class Z4FontSelectionPanel extends Z4AbstractValuePanel {
     this.fonts = fonts;
     Z4UI.addLabel(this, Z4Translations.FILTER, new GBC(0, 0).a(GBC.WEST));
     this.filter.addActionListener(event => {
-      let str = this.filter.getText();
+      let str = this.filter.getText().toLowerCase();
       this.radios.forEach((radio, index, array) => {
         radio.setSelected(false);
-        radio.getStyle().display = this.fonts[index].contains(str) ? "flex" : "none";
-        this.value = null;
-        this.onchange();
+        radio.getStyle().display = this.fonts[index].toLowerCase().indexOf(str) !== -1 ? "flex" : "none";
       });
+      this.onFontChange();
     });
-    this.add(this.filter, new GBC(0, 1).f(GBC.HORIZONTAL));
+    this.add(this.filter, new GBC(0, 1).f(GBC.HORIZONTAL).i(0, 0, 5, 0));
     let panel = new JSPanel();
     panel.cssAddClass("z4fontselectionpanel-fontlist");
     panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     this.add(panel, new GBC(0, 2));
     let buttonGroup = new ButtonGroup();
     fonts.forEach(font => this.addFont(font, buttonGroup, panel));
+    this.size.setSignVisible(false);
+    this.size.setRange(7, 400);
+    this.size.setValue(new Z4SignedValue(new Z4Sign(Z4SignBehavior.POSITIVE), 12));
+    this.size.addChangeListener(event => this.onFontChange());
+    this.add(this.size, new GBC(1, 0).h(2));
   }
 
    addFont(font, buttonGroup, panel) {
     let radio = new JSRadioButton();
     radio.setText(font);
-    radio.addActionListener(event => {
-      // this.value = new Z4Font(font, 0, true, true);
-      this.onchange();
-    });
+    radio.addActionListener(event => this.onFontChange());
+    this.radios.push(radio);
     buttonGroup.add(radio);
     panel.add(radio, null);
+  }
+
+   onFontChange() {
+    let index = this.radios.findIndex(radio => radio.isSelected() && radio.getStyle().display === "flex");
+    if (index !== -1) {
+      this.value = new Z4Font(this.fonts[index], this.size.getValue().getValue(), this.bold.isSelected(), this.italic.isSelected());
+    } else {
+      this.value = null;
+    }
+    this.onchange();
   }
 
    setValue(value) {
