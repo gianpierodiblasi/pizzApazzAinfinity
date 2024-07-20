cf6be0dbda047ca04d10f59789d5ccb23f0d39c9
gianpiero.diblasi <gianpiero.diblasi@PC-GianpieroDiBlasi>
2024-07-20 17:46:31 +0200

    shapes and paths

--- src/pizzapazza/ui/panel/math/geometricshape/Z4ShapesAndPathsPanel.java
+++ src/pizzapazza/ui/panel/math/geometricshape/Z4ShapesAndPathsPanel.java
@@ -1,7 +1,11 @@
 package pizzapazza.ui.panel.math.geometricshape;
 
+import javascript.awt.GBC;
 import javascript.awt.GridBagLayout;
+import javascript.swing.JSComponent;
 import javascript.swing.JSPanel;
+import pizzapazza.util.Z4Translations;
+import static simulation.js.$Globals.document;
 
 /**
  * The panel to manage shapes and paths
@@ -17,6 +21,18 @@ public class Z4ShapesAndPathsPanel extends JSPanel {
     super();
     this.cssAddClass("z4shapesandpathspanel");
     this.setLayout(new GridBagLayout());
+
+    JSComponent fieldset = new JSComponent(document.createElement("fieldset"));
+    this.add(fieldset, new GBC(0, 0).f(GBC.BOTH).wxy(1, 1));
+
+    JSComponent legend = new JSComponent(document.createElement("legend"));
+    legend.setContent(Z4Translations.SHAPES_AND_PATHS);
+    fieldset.appendChild(legend);
+
+    JSPanel panel = new JSPanel();
+    panel.cssAddClass("z4shapesandpathspanel-container");
+    panel.setLayout(new GridBagLayout());
+    fieldset.appendChild(panel);
   }
-  
+
 }
--- transpile/pizzapazza/ui/panel/math/geometricshape/Z4ShapesAndPathsPanel.js
+++ transpile/pizzapazza/ui/panel/math/geometricshape/Z4ShapesAndPathsPanel.js
@@ -12,5 +12,14 @@ class Z4ShapesAndPathsPanel extends JSPanel {
     super();
     this.cssAddClass("z4shapesandpathspanel");
     this.setLayout(new GridBagLayout());
+    let fieldset = new JSComponent(document.createElement("fieldset"));
+    this.add(fieldset, new GBC(0, 0).f(GBC.BOTH).wxy(1, 1));
+    let legend = new JSComponent(document.createElement("legend"));
+    legend.setContent(Z4Translations.SHAPES_AND_PATHS);
+    fieldset.appendChild(legend);
+    let panel = new JSPanel();
+    panel.cssAddClass("z4shapesandpathspanel-container");
+    panel.setLayout(new GridBagLayout());
+    fieldset.appendChild(panel);
   }
 }
